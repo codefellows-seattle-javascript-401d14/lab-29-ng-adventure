957dda5e2c1cd16cd81039c07334f03a7b56924b
Geno Guerrero <geno.guerrero@gmail.com>
2017-02-10 17:46:08 -0800

    angular app working and connected using npm run watch

---
+++ app/entry.js
@@ -0,0 +1,10 @@
+'use strict';
+
+require('./scss/base.scss');
+
+const angular = require('angular');
+
+angular.module('resloveAdventureApp', []);
+
+
+require('./component/adventure/adventure.js');
